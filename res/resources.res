aa176b079bfeb30a1aadff5f76e85ada9ebf1b7b
root <root@Michael-PC.localdomain>
2020-03-20 14:19:43 +0100

    player entity

--- res/resources.res
+++ res/resources.res
@@ -1,1 +1,2 @@
 IMAGE  background  "tiles/space.bmp" FAST
+SPRITE  ship    "sprites/imgship.bmp" 2 2 FAST 6
--- src/main.c
+++ src/main.c
@@ -11,19 +11,21 @@ typedef struct {
 	int velx;
 	int vely;
 	int health;
-	Sprite* sprite;
 	char name[6];
+    Sprite* sprite;
 } Entity;
+
+//player entity
+Entity player = {0, 0, 16, 16, 0, 0, 0, "PLAYER"};
 
 int main()
 {
-    //disable system inputs
-    SYS_disableInts();
-
     //load tile
+    SYS_disableInts();
     VDP_loadTileSet(background.tileset,1,DMA);
     //set his palette
     VDP_setPalette(PAL1, background.palette->data);
+    SYS_enableInts();
 
     //-random background
     //pixel index
@@ -44,24 +46,42 @@ int main()
         //to have more empty space tiles
         if(val > 3) val = 1;
         //display the tile
+        SYS_disableInts();
         VDP_setTileMapXY(PLAN_B,TILE_ATTR_FULL(PAL1,0,0,0,val), thex, they );
+        SYS_enableInts();
     }
 
     //store background offset
     int offset = 0;
     //set scrolling for moving background
+    SYS_disableInts();
     VDP_setScrollingMode(HSCROLL_PLANE,VSCROLL_PLANE);
+    SYS_enableInts();
+
+    //load sprite engine
+    SPR_init();
+
+    /*Add the player*/
+    player.x = 152;
+    player.y = 192;
+    player.health = 1;
+    player.sprite = SPR_addSprite(&ship,player.x,player.y,TILE_ATTR(PAL1,0,FALSE,FALSE));
+    SPR_update();
+
 	while(1)
 	{
         //scroll background
+        SYS_disableInts();
         VDP_setVerticalScroll(PLAN_B,offset -= 2);
         //to loop the background
         if(offset >= 256) offset = 0;
 
+        //update sprites
+        SPR_update();
+
 		VDP_waitVSync();
+        SYS_enableInts();
 	}
 
-    //re-enable system inputs
-    SYS_enableInts();
 	return(0);
 }
