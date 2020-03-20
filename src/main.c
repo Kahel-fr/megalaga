38aed234d072814672b492142a974af21e5f2fa4
root <root@Michael-PC.localdomain>
2020-03-20 14:47:09 +0100

    ennemies moving

--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 #include <resources.h>
 #include <kdebug.h>
 
+//edges of the screen
+#define LEFT_EDGE 0
+#define RIGHT_EDGE 320
+
 //number of ennemies
 #define MAX_ENEMIES 6
 
@@ -27,14 +31,39 @@ Entity enemies[MAX_ENEMIES];
 //count of ennemies still alive
 u16 enemiesLeft = 0;
 
+//kill an entity
 void killEntity(Entity* e){
 	e->health = 0;
 	SPR_setVisibility(e->sprite,HIDDEN);
 }
 
+//revive an entity
 void reviveEntity(Entity* e){
 	e->health = 1;
 	SPR_setVisibility(e->sprite,VISIBLE);
+}
+
+//update ennemies positions
+void positionEnemies(){
+    u16 i = 0;
+    //for each ennemy
+    for(i = 0; i < MAX_ENEMIES; i++){
+        Entity* e = &enemies[i];
+        //if the ennemy is still alive
+        if(e->health > 0){
+            //if the future position exceeds screen, change direction
+            if( (e->x+e->w) > RIGHT_EDGE){
+                e->velx = -1;
+            }
+            else if(e->x < LEFT_EDGE){
+                e->velx = 1;
+            }
+            //add velocity to current position
+            e->x += e->velx;
+            //set sprite position
+            SPR_setPosition(e->sprite,e->x,e->y);
+        }
+    }
 }
 
 int main()
@@ -118,6 +147,9 @@ int main()
         VDP_setVerticalScroll(PLAN_B,offset -= 2);
         //to loop the background
         if(offset >= 256) offset = 0;
+
+        //update enemies positions
+        positionEnemies();
 
         //update sprites
         SPR_update();
